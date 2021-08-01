77feccaae9de63ddeeff050c70d62e64fda1024d
cherryday <tcarevmaxim@gmail.com>
2021-08-01 20:42:20 +0300

    ColorBage component

---
+++ src/components/ColorBage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="bage"
+    :style="styles"
+    :class="classes"
+    @click="$emit('click', $event)"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'color-bage',
+  props: {
+    color: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 10
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    styles () {
+      return {
+        backgroundColor: this.color,
+        height: `${this.size}px`,
+        width: `${this.size}px`
+      }
+    },
+    classes () {
+      return {
+        'bage--active': this.active,
+        'bage--selected': this.selected
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bage {
+  border-radius: 50%;
+
+  &--selected {
+    cursor: pointer;
+  }
+  &--active {
+    border: 2px solid #525252;
+  }
+}
+</style>
---
+++ tests/unit/components/ColorBage.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount } from '@vue/test-utils'
+import ColorBage from '@/components/ColorBage'
+
+describe('ColorBage', () => {
+  let wrapper
+
+  const createComponent = (props) => {
+    wrapper = shallowMount(ColorBage, {
+      propsData: {
+        color: '#64C4ED',
+        ...props
+      }
+    })
+  }
+
+  it('emit event on click', () => {
+    createComponent()
+    wrapper.trigger('click')
+    expect(wrapper.emitted().click).toBeTruthy()
+  })
+
+  it.each([
+    ['active', 'bage--active'],
+    ['selected', 'bage--selected']
+  ])('add classes if props', (propsName, className) => {
+    createComponent({ [propsName]: true })
+    expect(wrapper.classes()).toContain(className)
+  })
+})
